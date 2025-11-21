f7db49e1a85865f70573cb30fc4be4fe77c98f58
RaviAnand Mohabir <moravrav@gmail.com>
2025-11-21 17:15:26 +0100

    docs: :memo: update config docs, add collapsible component for MDX (#9)
    Co-authored-by: Pablo Ramírez <pablor21@gmail.com>

--- docs/src/components.tsx
+++ docs/src/components.tsx
@@ -1,3 +1,26 @@
 "use client";
 
-export { Snippet, Alert, Link } from "@heroui/react";
+import { Accordion, AccordionItem } from "@heroui/react";
+import { ReactNode, useId } from "react";
+
+export const Collapsible = ({
+  title,
+  label,
+  children,
+}: {
+  title: string;
+  label: string;
+  children: ReactNode;
+}) => {
+  const id = useId();
+
+  return (
+    <Accordion variant="splitted" className="not-prose">
+      <AccordionItem key={id} aria-label={label} title={title}>
+        {children}
+      </AccordionItem>
+    </Accordion>
+  );
+};
+
+export { Accordion, AccordionItem, Alert, Link, Snippet } from "@heroui/react";
